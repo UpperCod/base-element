86891eee252e7a60e842c8b3268675bb914cc7ac
Uppercod <mtrujillo@outlook.cl>
2019-04-25 12:44:38 -0400

    now the bundle output is just module

--- rollup.config.js
+++ rollup.config.js
@@ -1,55 +1,16 @@
 import pkg from "./package.json";
 import size from "rollup-plugin-bundle-size";
-import resolve from "rollup-plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
-import sucrase from "rollup-plugin-sucrase";
-import del from "rollup-plugin-delete";
-import browsersync from "rollup-plugin-browsersync";
 
-let globals = {
-	"@atomico/core": "@atomico/core",
-	"@atomico/element": "@atomico/element"
-};
-
-let plugins = [
-	del({
-		targets: [pkg.module.replace(/\/[^\/]+$/, "")]
-	}),
-	resolve({
-		extensions: [".js", ".ts"]
-	}),
-	sucrase({
-		production: true,
-		exclude: ["node_modules/**"],
-		jsxPragma: "h",
-		transforms: ["typescript", "jsx"]
-	})
-];
-
-if (process.env.ROLLUP_WATCH) {
-	plugins.push(browsersync());
-} else {
-	process.env.BUILD = "production";
-	plugins.push(terser());
-}
-
-plugins.push(size());
+let plugins = [terser(), size()];
 
 export default {
 	input: pkg.source,
 	output: [
 		{
-			name: pkg.name,
-			file: pkg.unpkg,
-			format: "umd",
-			sourcemap: true,
-			globals
-		},
-		{
 			file: pkg.module,
-			format: "esm",
 			sourcemap: true,
-			globals
+			format: "es"
 		}
 	],
 	plugins
